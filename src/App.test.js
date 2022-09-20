0325f7670955af71d656e7577003529cddbfdf71
Weiyu Tang <twyla9876@outlook.com>
2022-09-20 13:16:25 +0100

    added counter component, added relative tests for it, changed the style for the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import Recipes from './Recipes'
+import AwesomeCounter from './AwesomeCounter';
 
 import { setupServer } from 'msw/node'
 import { rest } from 'msw'
@@ -58,4 +61,75 @@ test('displays error message when fetching recipes is unsuccessful', async () =>
 
   expect(await screen.findByText('Failed to fetch recipes')).toBeInTheDocument()
   expect(screen.queryByRole('listitem')).not.toBeInTheDocument()
+})
+
+test('Awesome Counter should have the correct title', () => {
+  render(<AwesomeCounter />)
+
+  expect(screen.getByRole('heading')).toHaveTextContent('Awesome Counter')
+})
+
+test('Awesome Counter should have the correct initial value when set to 7', () => {
+  render(<AwesomeCounter initialValue={7} />)
+  const count = screen.queryByText(7)
+  expect(count).toBeVisible()
+})
+
+test('Awesome Counter should have a default value of 0', () => {
+  render(<AwesomeCounter />)
+  const count = screen.queryByText(0)
+  expect(count).toBeVisible()
+})
+
+test('Awesome Counter should increase the value correctly when add is clicked once', () => {
+  render(<AwesomeCounter initialValue={1}/>)
+  const addButton = screen.getByText('Add')
+  userEvent.click(addButton)
+  const count = screen.queryByText(2)
+  expect(count).toBeVisible()
+})
+
+test('Awesome Counter should increase the value correctly when add is clicked twice', () => {
+  render(<AwesomeCounter initialValue={1}/>)
+  const addButton = screen.getByText('Add')
+  userEvent.click(addButton)
+  userEvent.click(addButton)
+  const count = screen.queryByText(3)
+  expect(count).toBeVisible()
+})
+
+test('Awesome Counter should decrease the value correctly when remove is clicked once', () => {
+  render(<AwesomeCounter initialValue={5}/>)
+  const removeButton = screen.getByText('Remove')
+  userEvent.click(removeButton)
+  const count = screen.queryByText(4)
+  expect(count).toBeVisible()
+})
+
+test('Awesome Counter should decrease the value correctly when remove is clicked twice', () => {
+  render(<AwesomeCounter initialValue={5}/>)
+  const removeButton = screen.getByText('Remove')
+  userEvent.click(removeButton)
+  userEvent.click(removeButton)
+  const count = screen.queryByText(3)
+  expect(count).toBeVisible()
+})
+
+test('Awesome Counter should not allow a negative number when the initial value is 0 and remove is clicked', () => {
+  render(<AwesomeCounter initialValue={0}/>)
+  const removeButton = screen.getByText('Remove')
+  userEvent.click(removeButton)
+  const count = screen.queryByText(0)
+  expect(count).toBeVisible()
+})
+
+test('Awesome Counter should not allow a negative number when the initial value is 2 and remove is clicked 4 times', () => {
+  render(<AwesomeCounter initialValue={2}/>)
+  const removeButton = screen.getByText('Remove')
+  userEvent.click(removeButton)
+  userEvent.click(removeButton)
+  userEvent.click(removeButton)
+  userEvent.click(removeButton)
+  const count = screen.queryByText(0)
+  expect(count).toBeVisible()
 })
